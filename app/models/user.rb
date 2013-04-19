92378d984bb148cd371e827c27b25e74c724b98a
Ben Klang <bklang@mojolingo.com>
2013-04-19 18:56:05 -0400

    Some basic abilities for admins

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   belongs_to :team
+  ROLES = %w[admin player observer]
 end
 
 # == Schema Information
