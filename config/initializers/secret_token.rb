76a1258653c188a349b30ea47397c748b6bd1704
Ben Klang <bklang@mojolingo.com>
2013-04-19 17:11:03 -0400

    rails new gac2014

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gac2014::Application.config.secret_token = '4f04aca3aad181790fc263c85ea3930dd8f25100a50a3ed65ce9c09eee4b90e2c23fb3c1786745439bf3ca874bad072aabf372b7134d528c4ddd3dc06d239b6e'
