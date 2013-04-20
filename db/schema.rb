44a2c8d1458aefb27658d54007fea55ff9dec26e
Ben Klang <bklang@mojolingo.com>
2013-04-19 20:41:58 -0400

    Users are now confirmable, update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130419232840) do
+ActiveRecord::Schema.define(:version => 20130420004037) do
+
+  create_table "checkins", :force => true do |t|
+    t.datetime "timestamp"
+    t.integer  "location_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "locations", :force => true do |t|
     t.string   "address"
@@ -29,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20130419232840) do
     t.datetime "updated"
     t.text     "description"
     t.string   "slogan"
+    t.string   "logo"
   end
 
   create_table "users", :force => true do |t|
@@ -51,8 +59,13 @@ ActiveRecord::Schema.define(:version => 20130419232840) do
     t.datetime "modified"
     t.string   "role"
     t.integer  "team_id"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
