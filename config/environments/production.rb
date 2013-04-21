3d1ca2220ce5eaf34360de756041706b4f55cac0
Ben Klang <bklang@mojolingo.com>
2013-04-20 23:59:57 -0400

    Add default host for links in email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Gac2014::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Specify the base URL for email notifications
+  config.action_mailer.default_url_options = { :host => 'gac2014.herokuapp.com' }
 end
 
 ActionMailer::Base.smtp_settings = {
