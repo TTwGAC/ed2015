5235d2095c56757d1dae1e7319d1e6889b881b28
Ben Klang <bklang@mojolingo.com>
2013-04-21 20:34:04 -0400

    Annotate User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,3 +22,34 @@ describe User do
     u.team.should == Team.first(conditions: {name: "Observers"})
   end
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  first_name             :string(255)
+#  last_name              :string(255)
+#  nickname               :string(255)
+#  created                :datetime
+#  modified               :datetime
+#  role                   :string(255)
+#  team_id                :integer
+#  confirmation_token     :string(255)
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string(255)
+#
+
