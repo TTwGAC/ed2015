8a49fd74b144260eac31e432a7e5301b96e21b5f
Ben Klang <bklang@mojolingo.com>
2013-05-05 21:56:48 -0400

    Fix user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  it "should set the role based on the team name" do
+  it "should set the role to admin when on the Game Control team" do
     u = FactoryGirl.build(:user)
     u.team = Team.first conditions: {name: "Game Control"}
     u.role.should == "admin"
+  end
 
+  it "should set the role to observer when on the Observers team" do
     u = FactoryGirl.build(:user)
     u.team = Team.first conditions: {name: "Observers"}
     u.role.should == "observer"
+  end
 
+  it "should set the role to player when on any other team" do
     u = FactoryGirl.build(:user)
     t = FactoryGirl.build(:team)
     u.team = t
     u.role.should == "player"
+  end
+
+  it "should set the role to none when not a real user" do
+    u = User.new
+    u.role.should == "none"
   end
 
   it "should automatically join the Observers team" do
