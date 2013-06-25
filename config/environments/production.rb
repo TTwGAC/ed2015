46446e4ffa1f40c7c0620d90c6e00882f9059257
Ben Klang <bklang@mojolingo.com>
2013-06-25 15:08:31 -0400

    Remove more deprecated config options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,10 +75,6 @@ Gac2014::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
   # Specify the base URL for email notifications
   config.action_mailer.default_url_options = { :host => 'gac2014.com' }
 end
