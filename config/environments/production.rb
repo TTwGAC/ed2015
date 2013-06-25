758edf0c6adb4406bdf45669cf7b9efff6b4c008
Ben Klang <bklang@mojolingo.com>
2013-06-25 14:30:54 -0400

    Fix some deprecation messages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ Gac2014::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Eager load classes
+  config.eager_load = true
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
