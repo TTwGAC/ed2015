2eedc2670db4796a629610ab45c14c692d1bd59f
Ben Klang <bklang@mojolingo.com>
2013-07-12 17:37:57 -0400

    Fix user model spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,7 @@ describe PlayersController do
 
   it "should find a player" do
     player = FactoryGirl.build(:player)
-    player.role.should == 'observer'
+    player.role.should == 'player'
   end
 
   it "should filter out private player data"
