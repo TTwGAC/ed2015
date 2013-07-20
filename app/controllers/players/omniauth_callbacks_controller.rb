37a8553bc4140e2c877371782cac9b59ccac6548
Ben Klang <bklang@mojolingo.com>
2013-07-20 18:39:29 -0400

    Do this in the model

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -1,9 +1,6 @@
 class Players::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def handle_callback(service, player)
     if player.persisted?
-      # Skip email for OAuth logins
-      player.confirm!
-
       # TODO: Check for required information - redirect to edit screen if missing
       sign_in_and_redirect player, :event => :authentication
       set_flash_message(:notice, :success, :kind => service) if is_navigational_format?
