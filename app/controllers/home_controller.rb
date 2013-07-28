965b4314aa2937673a40db8c2632bb93fcd7a609
Ben Klang <bklang@mojolingo.com>
2013-07-28 17:10:01 -0400

    Show teams on GCHQ map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    if can? :manage, :all
+      @map_data = Location.all.to_gmaps4rails do |loc, marker|
+        marker.infowindow render_to_string partial: '/locations/infowindow', locals: {loc: loc}
+        marker.picture({
+          width:  32,
+          height: 32,
+          picture: "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=#{loc.teams.count}|99FF99|000000"
+        })
+      end
+    end
   end
 end
