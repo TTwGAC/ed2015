a733cc06b87435afd37566892601dc1b3c32e746
Ben Klang <bklang@mojolingo.com>
2013-08-17 16:48:52 -0400

    Manually precompile JS for now :(

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,13 @@ Gac2014::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(
+    home.js
+    jquery.countdown.js
+    gmaps4rails.js
+    pregame_dashboard.js
+    puzzles.js
+  )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
