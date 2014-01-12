94818a4b064d1af23cefdb9b2b2160d905e7ab36
Ben Klang <bklang@mojolingo.com>
2014-01-12 16:06:56 -0500

    Fix having more than 26 locations in a cluster

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,15 +1,24 @@
 module LocationsHelper
+  def next_letter(letter)
+    case letter
+    when /[A-Y]/
+      letter.next
+    when nil, ''
+      'A'
+    else
+      ''
+    end
+  end
+
   def map_data(locations)
     @map_points ||= {}
     default_cluster = Cluster.new(name: 'Unclustered', color: 'red')
     locations.to_gmaps4rails do |loc, marker|
       cluster = loc.cluster || default_cluster
-      letter = if @map_points[cluster]
-        @map_points[cluster].keys.last.next
-      else
-        @map_points[cluster] ||= {}
-        'A'
-      end
+      @map_points[cluster] ||= {}
+
+      letter = next_letter @map_points[cluster].keys.last
+
       @map_points[cluster][letter] = loc
       marker.infowindow render(partial: '/locations/infowindow', locals: {loc: loc})
       marker.json name: loc.name, address: loc.address, picture: marker_img_path(cluster.color, letter)
