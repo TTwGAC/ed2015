0b025acc4559d739b701e28b24d2b49ee4cadc4d
Ben Klang <bklang@mojolingo.com>
2014-01-19 11:59:26 -0500

    Fix player sort (can't sort on a virtual column!)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,7 +8,7 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.json
   def index
-    @players = Player.find(:all, order: 'name')
+    @players = Player.find(:all, order: 'first_name')
 
     respond_to do |format|
       format.html # index.html.erb
