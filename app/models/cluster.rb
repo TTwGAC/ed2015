a316998359d8ec02d825646a3cf834efea8465b4
Ben Klang <bklang@mojolingo.com>
2014-01-20 17:27:09 -0500

    Validate name on Cluster

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -2,6 +2,7 @@ class Cluster < ActiveRecord::Base
   COLORS = %w[blue brown darkgreen green orange paleblue pink purple red yellow]
   has_many :locations
   validates_inclusion_of :color, in: COLORS
+  validates_presence_of :name
 end
 
 # == Schema Information
