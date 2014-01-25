9267b36d7976990a218793d41381a68e82a00e12
Ben Klang <bklang@mojolingo.com>
2014-01-24 21:37:54 -0500

    Move map helpers to application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'uri'
 require 'cgi'
 
@@ -51,5 +53,51 @@ module ApplicationHelper
     src.query = query.join "&"
 
     haml_tag :iframe, id: "ytplayer", type: "text/html", width: "640", height: "390", src: src.to_s, frameborder: "0"
+  end
+
+  def next_letter(letter)
+    case letter
+    when /[A-Y]/
+      letter.next
+    when nil, ''
+      'A'
+    else
+      ''
+    end
+  end
+
+  def map_data(locations)
+    logger.info "Prepping map data with locations #{locations}"
+    @map_points ||= {}
+    default_cluster = Cluster.new(name: 'Unclustered', color: 'red', sequence: 0)
+    locations.to_gmaps4rails do |loc, marker|
+      logger.debug "Processing location #{loc}"
+      cluster = loc.cluster || default_cluster
+      @map_points[cluster] ||= {}
+
+      letter = next_letter @map_points[cluster].keys.last
+
+      @map_points[cluster][letter] = loc
+      marker.infowindow render(partial: '/locations/infowindow', locals: {loc: loc})
+      marker.json name: loc.name, address: loc.address, picture: marker_img_path(cluster.color, letter)
+    end
+  end
+
+  def marker_img(color, letter)
+    image_tag marker_img_path(color, letter)
+  end
+
+  def marker_img_path(color, letter)
+    asset_path "map_markers/#{color}_Marker#{letter}.png"
+  end
+
+  def open_hours(location)
+    result = ""
+    if location.open_time || location.close_time
+      result << location.open_time.strftime("%l:%M %p") if location.open_time
+      result << (result.length > 0 ? " &mdash; " : " until ")
+      result << location.close_time.strftime("%l:%M %p") if location.close_time
+    end
+    result.html_safe
   end
 end
--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,45 +1,4 @@
+# encoding: utf-8
+
 module LocationsHelper
-  def next_letter(letter)
-    case letter
-    when /[A-Y]/
-      letter.next
-    when nil, ''
-      'A'
-    else
-      ''
-    end
-  end
-
-  def map_data(locations)
-    @map_points ||= {}
-    default_cluster = Cluster.new(name: 'Unclustered', color: 'red', sequence: 0)
-    locations.to_gmaps4rails do |loc, marker|
-      cluster = loc.cluster || default_cluster
-      @map_points[cluster] ||= {}
-
-      letter = next_letter @map_points[cluster].keys.last
-
-      @map_points[cluster][letter] = loc
-      marker.infowindow render(partial: '/locations/infowindow', locals: {loc: loc})
-      marker.json name: loc.name, address: loc.address, picture: marker_img_path(cluster.color, letter)
-    end
-  end
-
-  def marker_img(color, letter)
-    image_tag marker_img_path(color, letter)
-  end
-
-  def marker_img_path(color, letter)
-    asset_path "map_markers/#{color}_Marker#{letter}.png"
-  end
-
-  def open_hours(location)
-    result = ""
-    if location.open_time || location.close_time
-      result << location.open_time.strftime("%l:%M %p") if location.open_time
-      result << (result.length > 0 ? " &mdash; " : " until ")
-      result << location.close_time.strftime("%l:%M %p") if location.close_time
-    end
-    result.html_safe
-  end
 end
