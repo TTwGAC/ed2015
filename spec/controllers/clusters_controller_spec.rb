76c2d08474ddf826f4c3976ee6c98b1f8d594670
Ben Klang <bklang@mojolingo.com>
2014-01-25 19:44:51 -0500

    Fix attribute name

--- spec/controllers/clusters_controller_spec.rb
+++ spec/controllers/clusters_controller_spec.rb
@@ -23,7 +23,7 @@ describe ClustersController do
   # This should return the minimal set of attributes required to create a valid
   # Cluster. As you add validations to Cluster, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "name" => "MyString", "order" => 1, "color" => "blue" } }
+  let(:valid_attributes) { { "name" => "MyString", "sequence" => 1, "color" => "blue" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
