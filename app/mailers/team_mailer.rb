d6b104166cfd8f311a831fce0b91e5d24836f524
Ben Klang <bklang@mojolingo.com>
2014-02-01 14:00:57 -0500

    DRY subject tag

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -1,23 +1,24 @@
 class TeamMailer < ActionMailer::Base
   default from: "noreply@gac2014.com"
+  SUBJECT_TAG = '[GAC2014]'
 
   def notify_team_created(player, team, recipient)
     @player, @team, @recipient = player, team, recipient
-    mail to: recipient.email, from: player.email, subject: "[GAC2014] New team created: #{team.name}"
+    mail to: recipient.email, from: player.email, subject: "#{SUBJECT_TAG} New team created: #{team.name}"
   end
 
   def invitation_to_join(player, team, recipient)
     @player, @team, @recipient = player, team, recipient
-    mail to: recipient, from: player.email, subject: "[GAC2014] Invitation to join team #{team.name}"
+    mail to: recipient, from: player.email, subject: "#{SUBJECT_TAG} Invitation to join team #{team.name}"
   end
 
   def notify_member_joined(joining_player, notified_player, team)
     @joining_player, @team = joining_player, team
-    mail to: notified_player.email, subject: "[GAC2014] #{joining_player.nickname} has joined team #{team.name}"
+    mail to: notified_player.email, subject: "#{SUBJECT_TAG} #{joining_player.nickname} has joined team #{team.name}"
   end
 
   def send_puzzle(player, puzzle)
     @player, @puzzle = player, puzzle
-    mail to: player.email, subject: "[GAC2014] Your Next Puzzle: #{puzzle.name}"
+    mail to: player.email, subject: "#{SUBJECT_TAG} Your Next Puzzle: #{puzzle.name}"
   end
 end
