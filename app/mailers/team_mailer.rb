03bb7029faf4fb4d89d3ee78c2fefff4aaa83217
Ben Klang <bklang@mojolingo.com>
2014-02-01 13:58:02 -0500

    Use default from address

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -13,7 +13,7 @@ class TeamMailer < ActionMailer::Base
 
   def notify_member_joined(joining_player, notified_player, team)
     @joining_player, @team = joining_player, team
-    mail to: notified_player.email, from: 'gchq@gac2014.com', subject: "[GAC2014] #{joining_player.nickname} has joined team #{team.name}"
+    mail to: notified_player.email, subject: "[GAC2014] #{joining_player.nickname} has joined team #{team.name}"
   end
 
   def send_puzzle(player, puzzle)
