d7301cfbb4a76887bac20f305a235b5240d22969
Ben Klang <bklang@mojolingo.com>
2014-02-23 18:51:34 -0500

    Fix players setting their avatar

--- app/controllers/players/registrations_controller.rb
+++ app/controllers/players/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Players::RegistrationsController < Devise::RegistrationsController
   def create
     super
+    persist_avatar
     if resource.persisted?
       event "create", :player, resource.id, description: "#{resource.name} registered as a new player"
       save_thirdparty_creds(resource, session)
@@ -9,9 +10,19 @@ class Players::RegistrationsController < Devise::RegistrationsController
 
   def update
     super
+    persist_avatar
     if resource.persisted?
       event "update", :player, resource.id, description: "#{resource.name} updated his information"
       save_thirdparty_creds(resource, session)
+    end
+  end
+
+  def persist_avatar
+    if params['player']['avatar']
+      @player.avatar = params['player']['avatar']
+      unless @player.save
+        flash[:error] = @player.errors.full_messages.first
+      end
     end
   end
 end
