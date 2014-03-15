75e35379bfcc0bbf07b89d4370bb08b2b4f7075c
Ben Klang <bklang@mojolingo.com>
2014-03-15 18:09:49 -0400

    Add puzzle details to checkin confirmation page

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -13,8 +13,8 @@ class Checkin < ActiveRecord::Base
   delegate :name, to: :location, prefix: true
   delegate :name, to: :team, prefix: true
   delegate :name, to: :player, prefix: true
-  delegate :name, to: :solved_puzzle, prefix: true
-  delegate :name, to: :next_puzzle, prefix: true
+  delegate :name, :flavortext, :documents_for_players, to: :solved_puzzle, prefix: true
+  delegate :name, :flavortext, :documents_for_players, to: :next_puzzle, prefix: true
 
   validates_uniqueness_of :location_id, scope: :team_id
 
