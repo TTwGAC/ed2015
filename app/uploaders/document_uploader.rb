1fb37c5f3dc208d821ae3b564cee2c40d0b3288d
Ben Klang <bklang@mojolingo.com>
2014-03-22 13:09:57 -0400

    Be consistent with the file name

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -7,6 +7,6 @@ class DocumentUploader < CarrierWave::Uploader::Base
   process :set_content_type
 
   def filename
-    "#{File.basename(original_filename, File.extname(original_filename))}-#{model.get_token}#{File.extname(original_filename)}" if original_filename
+    "#{model.name}-#{model.get_token}#{File.extname(original_filename)}" if original_filename
   end
 end
