a2e36c6a8827a5c0d2652b1a126602444e973f63
Ben Klang <bklang@mojolingo.com>
2014-04-03 00:19:28 -0400

    Message Index and helpers

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,11 +9,29 @@ class Message < ActiveRecord::Base
   validates :delivery_type, inclusion: { in: DELIVERY_TYPES }, presence: true
   validates :status, inclusion: { in: STATUSES }
   validates :destination, presence: true
+  delegate :name, to: :sender, prefix: true
 
   before_validation :default_status
 
   def default_status
     self.status ||= 'draft'
   end
+
+  def sendable?
+    self.status == 'draft'
+  end
+
+  def destination_object
+    klass, id = self.destination.split ':'
+    valid_classes = %w(Team Player)
+    if valid_classes.include? klass
+      Kernel.const_get(klass).find(id)
+    elsif id == 'everyone'
+      OpenStruct.new name: 'Everyone'
+    else
+      OpenStruct.new name: "UNKNOWN OBJECT"
+    end
+  end
+
 
 end
