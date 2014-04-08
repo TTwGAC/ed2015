5b80bf5818f525f7d620fe0cd042f68bad182823
Ben Klang <bklang@mojolingo.com>
2014-04-08 18:57:05 -0400

    Get Radio GAC stats for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require 'faraday'
 
 class DashboardController < ApplicationController
   before_filter :authenticate_player!
@@ -15,6 +16,7 @@ class DashboardController < ApplicationController
 
   def game_control_dashboard
     @locations = Location.all
+    @radio_gac = get_radio_gac_info
     respond_to do |format|
       format.html do
         # Render the UI
@@ -35,6 +37,43 @@ class DashboardController < ApplicationController
         render :player_dashboard
       end
     end
+  end
+
+private
+
+  def get_radio_gac_info
+    response = icecast_api('/admin/stats')
+    logger.warn "Icecast response: #{response.inspect}"
+    stats = {}
+    if response.status == 200
+      xml = Nokogiri::XML(response.body)
+      if xml.xpath("/icestats/source[@mount='/radio_gac']").present?
+        channel = 'radio_gac'
+      else
+        channel = 'auto-dj'
+      end
+
+      stats[:listeners] = xml.xpath("/icestats/source[@mount='/#{channel}']/listeners").text.to_i
+      stats[:track]     = xml.xpath("/icestats/source[@mount='/#{channel}']/title").text
+    else
+      # The server returns 400 when no users are connected
+      stats[:listeners] = 0
+      stats[:track]     = "Unknown"
+    end
+
+    stats
+  end
+
+  def icecast_api(query, params = {})
+    unless @icecast_api
+      @icecast_api = Faraday.new(url: 'http://gchq.gac2014.com:8000') do |faraday|
+        faraday.response :logger
+        faraday.adapter Faraday.default_adapter
+      end
+      @icecast_api.basic_auth ENV['ICECAST_USERNAME'], ENV['ICECAST_PASSWORD']
+    end
+
+    @icecast_api.get query, params
   end
 
 end
