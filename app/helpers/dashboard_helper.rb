801c5774336567ef89fc3154a7e4f9eb37ecd139
Ben Klang <bklang@mojolingo.com>
2014-04-13 17:58:59 -0400

    Sort DNF last

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,6 +2,12 @@
 
 module DashboardHelper
   def teams_sorted_by_score
-    Team.playing.sort_by {|team| team.score}
+    Team.playing.sort_by do |team|
+      if team.score <= 0
+        99999999999
+      else
+        team.score
+      end
+    end
   end
 end
