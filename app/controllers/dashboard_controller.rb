79be141c07852f01da219209ef65dc6c519a1c4e
Ben Klang <bklang@mojolingo.com>
2014-04-12 20:33:24 -0400

    Show Best Team too

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -101,8 +101,11 @@ private
     stats[:total] = Penalty.all.count
     stats[:average] = Penalty.all.average(:minutes)
     team_penalties = Penalty.all.group(:team_id).sum(:minutes)
-    team_id, minutes = team_penalties.sort_by { |team_id, minutes| minutes }.last
+    team_penalties = team_penalties.sort_by { |team_id, minutes| minutes }
+    team_id, minutes = team_penalties.last
     stats[:worst_team] = "#{Team.find(team_id).name}: #{minutes}"
+    team_id, minutes = team_penalties.first
+    stats[:best_team] = "#{Team.find(team_id).name}: #{minutes}"
     stats
   end
 
